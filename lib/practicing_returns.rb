f72fc45e02fa432c6b1280adc2cc7bbbab63d6dc
Alie Stone <helloaliestone@gmail.com>
2020-12-15 23:50:11 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,10 +1,11 @@
 require 'pry'
 
-binding.pry
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
